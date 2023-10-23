6af01ecc19eb0c08d9876d42ea1bf1cdacef5beb
krismantii <krismantii@gmail.com>
2023-10-23 13:16:08 +0700

    cucumber update

--- src/test/java/cucumber/stepDef/login.java
+++ src/test/java/cucumber/stepDef/login.java
@@ -71,5 +71,6 @@ public class login {
         String error = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")).getText();
         Assert.assertEquals(error, "Epic sadface: Username and password do not match any user in this service");
     }
+
 }
 
---
+++ src/test/java/cucumber/stepDef/tambahProduk.java
@@ -0,0 +1,23 @@
+package cucumber.stepDef;
+
+public class tambahProduk {
+    @cucumber.api.java.en.Given("User berada di halaman produk")
+    public void userBeradaDiHalamanProduk() {
+    }
+
+    @cucumber.api.java.en.When("User klik tombol add to cart")
+    public void userKlikTombolAddToCart() {
+    }
+
+    @cucumber.api.java.en.And("User berhasil menambahkan produk")
+    public void userBerhasilMenambahkanProduk() {
+    }
+
+    @cucumber.api.java.en.Then("User klik icon keranjang")
+    public void userKlikIconKeranjang() {
+    }
+
+    @cucumber.api.java.en.And("User melihat menu keranjang")
+    public void userMelihatMenuKeranjang() {
+    }
+}
